f030dc38d282a87234b01adfd14de7552ddf6e8b
Efrain Lemus <killdragon7257@gmail.com>
2023-03-02 14:51:12 -0800

    Fixed crash

--- grade.sh
+++ grade.sh
@@ -4,7 +4,7 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if [[ ! -f student-submission/ListExamples.java ]] then
+if [[ ! -f student-submission/ListExamples.java ]]; then
     echo 'File ListExamples.java could not be found.'
     exit 1
 fi
@@ -13,14 +13,14 @@ cd student-submission
 cp ../TestListExamples.java ./
 cp -r ../lib ./
 javac -cp $CPATH *.java
-if [[ $? -ne 0 ]] then
+if [[ $? -ne 0 ]]; then
     echo 'Could not compile your code'
     exit 1
 fi
 echo 'Compiled Successfully.'
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples &>junit.txt
 
-TESTS=$(sed '2q;d' junit.txt | grep -o '.' | wc -)
+TESTS=$(sed '2q;d' junit.txt | grep -o '.' | wc -l)
 echo $TESTS
 
 #FAILS=$(grep "failure" ./junit.txt | grep -o -E '[0-9]+')
